2a11c3109ad6d3e35c23eb3365ab05e85a3fd51e
moulali <moulali123@gamil.com>
2022-04-11 11:04:11 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,16 @@
+public class test {
+    int a;
+    int b;
+   public int c;
+
+    public  void sum(int a,int b){
+        c=a+b;
+        System.out.println(c);
+
+    }
+    public static void main(String[] args){
+       test obj=new test();
+       obj.sum(10,20);
+
+    }
+}
---
+++ test2.java
@@ -0,0 +1,8 @@
+public class test2 {
+   static String a= "moulali";
+   static String b="mahesh";
+    static String c=a+b;
+    public static void main(String[] args){
+        System.out.println(c);
+    }
+}
